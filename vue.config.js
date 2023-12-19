a7d65f123a2bbc69b2986ff8580c5f2e47727f90
zhangyue44 <zy1617768774@163.com>
2023-12-19 16:54:50 +0800

    feat: proxy配置

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,19 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "",
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+        onProxyReq(proxyReq, req, res) {
+          proxyReq.setHeader("Cookie", "poxxxxx....");
+        },
+      },
+    },
+  },
 });
